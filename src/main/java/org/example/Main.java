0682496354bc45a1360c3e94c8268f7519e3ea38
SibelK-tcd-lab <sibelkarakelle@gmail.com>
2026-04-28 01:21:06 +0300

    feat: all methods implemented and clean code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Kodlarını burada test edebilirsin
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
